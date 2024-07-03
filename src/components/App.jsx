8558bb4e5dbc4f35f26bc6fa0cd0b80d3e16061a
Estera-Mieszczak <144910099+Estera-Mieszczak@users.noreply.github.com>
2024-07-03 21:24:54 +0200

    Working app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
+// import './App.css'
+import { ContactList } from './ContactList/ContactList';
+import { Form } from './Form/Form';
+import { ContactFilter } from './ContactFilter/ContactFilter';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from '../redux/operation';
+import { getError, getIsLoading } from '../redux/selectors';
+
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
+  const error = useSelector(getError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+  
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Form />
+      <ContactFilter />
+      {isLoading && !error && <b>Request in progress...</b>}
+      {/* {error && <p>{error}</p>} */}
+      <ContactList />
     </div>
   );
 };
---
+++ src/components/Form/Form.js
@@ -0,0 +1,28 @@
+import css from './Form.module.css';
+// import { Button } from "components/Button/Button";
+import { useDispatch } from "react-redux";
+import { addContact } from "../../redux/operation";
+
+export const Form = () => {
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    const form = event.target;
+    const name = form.elements.name.value;
+    const phone = form.elements.phone.value;
+    dispatch(addContact({ name: name, phone: phone }));
+    form.reset();
+  };
+  
+  return (
+    <form className={css.contactForm} onSubmit={handleSubmit}>
+      <label htmlFor='nameContact'>Name</label> 
+      <input className={css.input} id="nameContact" type="text" name="name" pattern="^[a-zA-Zа-яА-Я]*$" required placeholder="Name"></input>
+      <label htmlFor='numberContact'>Phone number</label> 
+      <input className={css.input} id="numberContact" type="tel" name="phone" pattern="\+?\d{1,4}?[ .\\-\\s]?\(?\d{1,3}?\)?[ .\\-\\s]?\d{1,4}[ .\\-\\s]?\d{1,4}[ .\\-\\s]?\d{1,9}" required placeholder="Number"></input>
+      <button type="submit">Add contact</button>
+    </form>
+  )
+};
